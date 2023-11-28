d2c465acf15d6591ed8920295ecb73dac4b7c42e
Carlos Mourão <carlosmourao2310@gmail.com>
2023-11-28 16:50:46 -0300

    Aula 05

--- api_social_books/src/main/java/br/fepi/socialbooks/resources/LivrosResources.java
+++ api_social_books/src/main/java/br/fepi/socialbooks/resources/LivrosResources.java
@@ -10,46 +10,58 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.fepi.socialbooks.domain.Comentario;
 import br.fepi.socialbooks.domain.Livro;
+import br.fepi.socialbooks.repository.ComentariosRepository;
 import br.fepi.socialbooks.repository.LivrosRepository;
 
 @RestController
 @RequestMapping(value = "/livros")
 public class LivrosResources {
 	
-	@Autowired 
+	@Autowired
 	private LivrosRepository livrosRepository;
+	
+	@Autowired
+	private ComentariosRepository comentariosRepository;
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public List<Livro> listar() {
 		return livrosRepository.findAll();
+	}
 	
-		
-}
 	@RequestMapping(method = RequestMethod.POST)
 	public void salvar(@RequestBody Livro livro) {
 		livrosRepository.save(livro);
-		
+	}
 	
-	}
 	@CrossOrigin
-	@RequestMapping(value = "/{id}",method = RequestMethod.GET)
-	public Livro buscaId(@PathVariable ("id")Long id) {
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public Livro buscaId(@PathVariable("id")
+			Long id) {
 		return livrosRepository.findById(id).orElse(null);
-		
-		
 	}
+	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public void deletar(@PathVariable("id")Long id) {
+	public void deletar (@PathVariable("id") Long id) {
 		livrosRepository.deleteById(id);
 	}
+	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public void atualizar (@RequestBody Livro livro,@PathVariable("id") Long id) {
+	public void atualizar (@RequestBody Livro livro, @PathVariable("id") Long id) {
 		livro.setId(id);
 		livrosRepository.save(livro);
+	}
+	
+	@RequestMapping(value = "/{id}/comentarios",method = RequestMethod.POST)
+	public void adicionarComentario(@PathVariable ("id")Long livroId,@RequestBody Comentario comentario)
+	{
+		Livro livro = new Livro();
+		livro.setId(livroId);
+		comentario.setLivro(livro);
+		comentariosRepository.save(comentario);
 		
 	}
-
 	
 	
 	
